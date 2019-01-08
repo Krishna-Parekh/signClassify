010d039b60253cb8571c52fb25036a76c3662502
Krishna-Parekh <46160969+Krishna-Parekh@users.noreply.github.com>
2019-01-08 20:54:51 +0530

    Add files via upload
    GCP ML-Engine Deployment package

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['keras==2.2.4',
+                     'Keras-Preprocessing==1.0.5',
+                     #'keras-utils==1.0.13',
+                     'Pillow==5.3.0',
+                     'h5py==2.9.0',
+                     'numpy==1.15.4',
+                     'scikit-learn==0.20.2']
+
+setup(
+  name='trainer',
+  version='0.1',
+  author = 'Krishna Parekh',
+  install_requires=REQUIRED_PACKAGES,
+  packages=find_packages(),
+  include_package_data=True,
+  requires=[],
+  description='CMLE Signature Classification',
+)
